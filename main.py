66a2257e1c5d3fb6e4667dd986855c7f06f92e0d
felixwhite <klassemax@gmail.com>
2023-05-14 18:01:24 +0800

    simple query parameter tests

--- main.py
+++ main.py
@@ -2,7 +2,41 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id : int):
+    return {"item_id": item_id}
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
