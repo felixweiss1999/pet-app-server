cf888380c4a68bd87a9eb5273bcd8cccb4ca4a3f
felixwhite <klassemax@gmail.com>
2023-05-15 23:59:01 +0800

    added jwt_handling

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,2 +1,28 @@
 #This file is responsible for signing, encoding
 # and returning JWTs
+import time, jwt
+from decouple import config
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+# returns the generated tokens (JWTs)
+def token_response(token: str):
+    return {
+        "access token" : token
+    }
+
+def signJWT(userID: str):
+    payload = {
+        "userID" : userID,
+        "expiry" : time.time() + 600
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm = JWT_ALGORITHM)
+    return token_response(token)
+
+def decodeJWT(token : str):
+    try:
+        decode_token = jwt.decode(token, JWT_SECRET, algorithm=JWT_ALGORITHM)
+        return decode_token if decode_token["expires"] >= time.time() else None
+    except:
+        return {}
