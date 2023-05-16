8777cf745a5b5e0da220a185c73a46d3021aed12
felixwhite <klassemax@gmail.com>
2023-05-16 20:34:56 +0800

    commented out schemas.py

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -5,31 +5,26 @@ class ItemBase(BaseModel):
     title: str
     description: str | None = None
 
-
 class ItemCreate(ItemBase):
     pass
 
-
 class Item(ItemBase):
     id: int
-    owner_id: int
+    owner_id: str
 
     class Config:
         orm_mode = True
 
 
-class UserBase(BaseModel):
+class UserBase(BaseModel): #common data while creating or reading data!
     email: str
 
-
-class UserCreate(UserBase):
+class UserCreate(UserBase): 
     password: str
 
-
-class User(UserBase):
-    id: int
+class User(UserBase): #these are for reading data!
     is_active: bool
-    items: list[Item] = []
+    items: list[Item] = [] #uses lazy eval!
 
-    class Config:
+    class Config: #.. because it is told here to do so! And also will know that this is not a dict, but an orm model to read out!
         orm_mode = True
