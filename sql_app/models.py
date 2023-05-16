37b4f7ff9f704fe803c39f998292f46155b053d0
felixwhite <klassemax@gmail.com>
2023-05-16 20:47:06 +0800

    added interface functions in crud.py

--- sql_app/models.py
+++ sql_app/models.py
@@ -19,6 +19,6 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner_id = Column(Integer, ForeignKey("users.email"))
 
     owner = relationship("User", back_populates="items")
