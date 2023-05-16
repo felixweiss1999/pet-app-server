9e9e75b3d299e15024d95d1459be81d06263a174
felixwhite <klassemax@gmail.com>
2023-05-16 20:23:00 +0800

    added sqlalchemy models

--- sql_app/database.py
+++ sql_app/database.py
@@ -6,8 +6,10 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} #argument needed for sqlite
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#this in itself is not a session yet, but every instance of SessionLocal will be
 
 Base = declarative_base()
+#will later inherit from this class to create each of the databse ORM models
---
+++ sql_app/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+#classes that inherit from Base are the SQLAlchemy models!
+class User(Base):
+    __tablename__ = "users"
+    email = Column(Integer, primary_key=True, index=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
