493c377faa5485889ef499926baecb4821ab25d9
felixwhite <klassemax@gmail.com>
2023-05-22 21:00:23 +0800

    cleanup

--- main.py
+++ main.py
@@ -28,11 +28,12 @@ def greet():
     return {"Hello", "World"}
 
 #posts
+
 @app.get("/posts", tags=["posts"], response_model=list[schemas.Post])
 def get_all_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_posts(db=db, skip=skip, limit=limit)
 
-@app.get("/posts/{id}", tags=["posts"], response_model=schemas.Post)
+@app.get("/posts/{post_id}", tags=["posts"], response_model=schemas.Post)
 def get_post_by_id(id: int, db: Session = Depends(get_db)):
     db_post = crud.get_post_by_id(db=db, id=id)
     if db_post is None:
@@ -46,7 +47,7 @@ def get_post_by_userid(user_id : str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found!")
     return db_user.posts
 
-@app.get("/posts/{id}/replies", tags=["posts"], response_model=list[schemas.Post])
+@app.get("/posts/{post_id}/replies", tags=["posts"], response_model=list[schemas.Post])
 def get_replies_to_post(postid : int, db: Session = Depends(get_db)):
     return crud.get_post_replies(db=db, postid=postid)
 
@@ -61,38 +62,8 @@ async def add_post(post: schemas.PostCreate, request : Request, db: Session = De
         raise HTTPException(status_code=403, detail="User does not exist!")
     return crud.create_post(db=db, post=post)
 
-
-
-
-
-#user signup - create new user
-@app.post("/user/signup", tags=["user"], response_model=schemas.User)
-def user_signup(user: schemas.UserCreate, db: Session = Depends(get_db)): #!!! lookup what this Body thing is in docs!
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="User already exists!")
-    return crud.create_user(db=db, user=user) #for this to work response model must be declared!
-    
-@app.post("/user/login", tags=["user"])
-def user_login(user: schemas.UserLogin, db: Session = Depends(get_db)): #UserCreate has password!
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        return signJWT(user.email)
-    raise HTTPException(status_code=403, detail="Invalid login credentials!")
-
-@app.get("/user/{user_id}", response_model=schemas.User, tags=["user"]) #User does not contain password!
-async def get_user_by_email(user_id: str, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-
-
-#file
-@app.post("/file", dependencies=[Depends(jwtBearer())], tags=["file"])
-async def create_file_for_post(post_id: int, fileending: str, file: UploadFile, request : Request, db: Session = Depends(get_db)):
+@app.post("/posts/{post_id}/file", dependencies=[Depends(jwtBearer())], tags=["posts"])
+async def add_file_to_post(post_id: int, fileending: str, file: UploadFile, request : Request, db: Session = Depends(get_db)):
     #check if post exists
     post = crud.get_post_by_id(db=db, id=post_id)
     if post == None:
@@ -116,15 +87,52 @@ async def create_file_for_post(post_id: int, fileending: str, file: UploadFile, 
     await file.close()
     return {"id": db_file.id}
 
-@app.post("/file", dependencies=[Depends(jwtBearer())], tags=["file"])
-async def create_file_as_profile_picture(email: str, fileending: str, file: UploadFile, request : Request, db: Session = Depends(get_db)):
+@app.get("/posts/{post_id}/file", response_class=FileResponse, tags=["posts"]) #FileResponse will handle everything from just returning the path to the file!
+async def get_file_by_post(post_id: int, db: Session = Depends(get_db)):
+    db_file = crud.get_filename_by_post(db=db, postid=post_id)
+    if db_file is None:
+        raise HTTPException(status_code=404, detail="Post does not have any files")
+    mimetype = mimetypes.guess_type(db_file.file_path)[0]
+    print(mimetype)
+    if mimetypes is None:
+        return FileResponse(path=db_file.file_path)
+    return FileResponse(path=db_file.file_path, content_disposition_type=mimetype)
+
+
+
+
+#user 
+
+@app.post("/user/signup", tags=["user"], response_model=schemas.User)
+def user_signup(user: schemas.UserCreate, db: Session = Depends(get_db)): #!!! lookup what this Body thing is in docs!
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="User already exists!")
+    return crud.create_user(db=db, user=user) #for this to work response model must be declared!
+    
+@app.post("/user/login", tags=["user"])
+def user_login(user: schemas.UserLogin, db: Session = Depends(get_db)): #UserCreate has password!
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        return signJWT(user.email)
+    raise HTTPException(status_code=403, detail="Invalid login credentials!")
+
+@app.get("/user/{user_id}", response_model=schemas.User, tags=["user"]) #User does not contain password!
+async def get_user_by_email(user_id: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.post("/user/{user_id}/profile_picture", dependencies=[Depends(jwtBearer())], tags=["user"])
+async def upload_profile_picture(user_id: str, fileending: str, file: UploadFile, request : Request, db: Session = Depends(get_db)):
     #check if post owner is currently logged-in user
     useMeToExtract = jwtBearer()
     confirmeduid : str = await useMeToExtract(request=request)
-    if email != confirmeduid:
+    if user_id != confirmeduid:
         raise HTTPException(status_code=403, detail="Cannot upload profile picture of someone else!")
     
-    db_file = crud.create_file(db=db,file=schemas.FileCreate(post=None, file_ending=fileending, user=email))
+    db_file = crud.create_file(db=db,file=schemas.FileCreate(post=None, file_ending=fileending, user=user_id))
     crud.set_filepath(db=db, fileid=db_file.id, filepath=f"files/{db_file.id}.{db_file.file_ending}")
     try:
         async with aiofiles.open(f"files/{db_file.id}.{db_file.file_ending}", 'wb') as out_file:
@@ -137,6 +145,25 @@ async def create_file_as_profile_picture(email: str, fileending: str, file: Uplo
     await file.close()
     return {"id": db_file.id}
 
+@app.get("/user/{user_id}/profile_picture", response_class=FileResponse, tags=["user"]) #FileResponse will handle everything from just returning the path to the file!
+async def get_profile_picture(user_id: str, db: Session = Depends(get_db)):
+    db_file = crud.get_filename_by_user(db=db, userid=user_id)
+    if db_file is None:
+        raise HTTPException(status_code=404, detail="User does not have a profile picture!")
+    mimetype = mimetypes.guess_type(db_file.file_path)[0]
+    print(mimetype)
+    if mimetypes is None:
+        return FileResponse(path=db_file.file_path)
+    return FileResponse(path=db_file.file_path, content_disposition_type=mimetype)
+
+
+
+
+
+
+
+#file
+
 @app.get("/file/{file_id}", response_class=FileResponse, tags=["file"]) #FileResponse will handle everything from just returning the path to the file!
 async def get_file_by_id(file_id: int, db: Session = Depends(get_db)):
     db_file = crud.get_filename_by_id(db=db, fileid=file_id)
@@ -148,25 +175,7 @@ async def get_file_by_id(file_id: int, db: Session = Depends(get_db)):
         return FileResponse(path=db_file.file_path)
     return FileResponse(path=db_file.file_path, content_disposition_type=mimetype)
 
-@app.get("/{post_id}/file", response_class=FileResponse, tags=["file"]) #FileResponse will handle everything from just returning the path to the file!
-async def get_file_by_post(post_id: int, db: Session = Depends(get_db)):
-    db_file = crud.get_filename_by_post(db=db, postid=post_id)
-    if db_file is None:
-        raise HTTPException(status_code=404, detail="Post does not have any files")
-    mimetype = mimetypes.guess_type(db_file.file_path)[0]
-    print(mimetype)
-    if mimetypes is None:
-        return FileResponse(path=db_file.file_path)
-    return FileResponse(path=db_file.file_path, content_disposition_type=mimetype)
 
-@app.get("/{user_id}/profile_picture", response_class=FileResponse, tags=["file"]) #FileResponse will handle everything from just returning the path to the file!
-async def get_profile_picture(user_id: str, db: Session = Depends(get_db)):
-    db_file = crud.get_filename_by_user(db=db, userid=user_id)
-    if db_file is None:
-        raise HTTPException(status_code=404, detail="User does not have a profile picture!")
-    mimetype = mimetypes.guess_type(db_file.file_path)[0]
-    print(mimetype)
-    if mimetypes is None:
-        return FileResponse(path=db_file.file_path)
-    return FileResponse(path=db_file.file_path, content_disposition_type=mimetype)
+
+
 
